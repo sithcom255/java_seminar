8eb74654caa288d7583bdb70eefcbe477e86459a
Jan Gavlik <x455794@mail.muni.cz>
2020-04-14 16:05:27 +0200

    tests passed

--- src/main/java/cz/muni/fi/pb162/project/utils/SimpleMath.java
+++ src/main/java/cz/muni/fi/pb162/project/utils/SimpleMath.java
@@ -1,6 +1,6 @@
 package cz.muni.fi.pb162.project.utils;
 
-import cz.muni.fi.pb162.project.geometry.Triangle;
+import cz.muni.fi.pb162.project.geometry.Polygon;
 import cz.muni.fi.pb162.project.geometry.Vertex2D;
 
 import java.util.Arrays;
@@ -10,48 +10,59 @@ import java.util.OptionalDouble;
  */
 public class SimpleMath {
     /**
-     * @param triangle takes a triangle
+     * @param polygon takes a polygon
      * @return min X value
      */
-    public static double minX(Triangle triangle) {
-        OptionalDouble minX = Arrays.stream(new Vertex2D[]{triangle.getVertex(0),
-                triangle.getVertex(1), triangle.getVertex(2)})
+    public static double minX(Polygon polygon) {
+        Vertex2D[] arr=getArray(polygon);
+        OptionalDouble minX = Arrays.stream(arr)
                 .mapToDouble(Vertex2D::getX)
                 .min();
         return  minX.getAsDouble();
     }
     /**
-     * @param triangle takes a triangle
+     * @param polygon takes a polygon
      * @return min y value
      */
-    public static double minY(Triangle triangle) {
-        OptionalDouble minX = Arrays.stream(new Vertex2D[]{triangle.getVertex(0),
-                triangle.getVertex(1), triangle.getVertex(2)})
+    public static double minY(Polygon polygon) {
+        Vertex2D[] arr=getArray(polygon);
+        OptionalDouble minX = Arrays.stream(arr)
                 .mapToDouble(Vertex2D::getY)
                 .min();
         return  minX.getAsDouble();
     }
     /**
-     * @param triangle takes a triangle
+     * @param polygon takes a polygon
      * @return max X value
      */
-    public static double maxX(Triangle triangle) {
-        OptionalDouble minX = Arrays.stream(new Vertex2D[]{triangle.getVertex(0),
-                                                            triangle.getVertex(1),
-                                                            triangle.getVertex(2)})
+    public static double maxX(Polygon polygon) {
+        Vertex2D[] arr=getArray(polygon);
+        OptionalDouble minX = Arrays.stream(arr)
                 .mapToDouble(Vertex2D::getX)
                 .max();
         return  minX.getAsDouble();
     }
     /**
-     * @param triangle takes a triangle
+     * @param polygon takes a polygon
      * @return max Y value
      */
-    public static double maxY(Triangle triangle) {
-        OptionalDouble minX = Arrays.stream(new Vertex2D[]{triangle.getVertex(0),
-                triangle.getVertex(1), triangle.getVertex(2)})
+    public static double maxY(Polygon polygon) {
+        Vertex2D[] arr=getArray(polygon);
+        OptionalDouble minX = Arrays.stream(arr)
                 .mapToDouble(Vertex2D::getY)
                 .max();
         return  minX.getAsDouble();
+    }
+
+    /**
+     * @param polygon polygon
+     * @return array of its vertexes
+     */
+    public static Vertex2D[] getArray(Polygon polygon){
+        Vertex2D[] arr=new Vertex2D[polygon.getNumVertices()];
+        for (int i=0;i<polygon.getNumVertices();i++){
+            arr[i]=polygon.getVertex(i);
+        }
+        return arr;
     }
 }
